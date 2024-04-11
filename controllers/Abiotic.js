fae932733a8949ee1a3bc754470d29cfbf224191
prabhukumar <prabhukumara2001@gmail.com>
2024-04-10 21:13:22 -0500

    done

--- controllers/Abiotic.js
+++ controllers/Abiotic.js
@@ -1,39 +1,50 @@
-
 var Abiotic = require('../models/Abiotic');
-// List of allAbiotic
-exports.Abiotic_list = function (req, res) {
-    res.send('NOT IMPLEMENTED:Abiotic list');
+// List of all Abiotic
+exports.Abiotic_list = function(req, res) {
+ res.send('NOT IMPLEMENTED: Abiotic list');
 };
 // for a specific Abiotic.
 exports.Abiotic_detail = async function(req, res) {
-    console.log("detail" + req.params.id)
+console.log("detail" + req.params.id)
+try {
+result = await Abiotic.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
+ 
+// Handle Abiotic create on POST.
+exports.Abiotic_create_post = function(req, res) {
+ res.send('NOT IMPLEMENTED: Abiotic create POST');
+};
+// Handle Abiotic delete from on DELETE.
+exports.Abiotic_delete = function(req, res) {
+ res.send('NOT IMPLEMENTED: Abiotic delete DELETE ' + req.params.id);
+};
+// Handle Abiotic delete on DELETE.
+exports.Abiotic_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
     try {
-    result = await Abiotic.findById( req.params.id)
+    result = await Abiotic.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
     res.send(result)
-    } catch (error) {
+    } catch (err) {
     res.status(500)
-    res.send(`{"error": document for id ${req.params.id} not found`);
+    res.send(`{"error": Error deleting ${err}}`);
     }
     };
-    
-// HandleAbiotic create on POST.
-exports.Abiotic_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED:Abiotic create POST');
-};
-// HandleAbiotic delete from on DELETE.
-exports.Abiotic_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED:Abiotic delete DELETE ' + req.params.id);
-};
-
-// HandleAbiotic update form on PUT.
+   
+// Handle Abiotic update form on PUT.
 exports.Abiotic_update_put = async function(req, res) {
 console.log(`update on id ${req.params.id} with body
 ${JSON.stringify(req.body)}`)
 try {
 let toUpdate = await Abiotic.findById( req.params.id)
 // Do updates of properties
-if(req.body.Abiotic_name)
-toUpdate.Abiotic_name = req.body.Abiotic_name;
+if(req.body.name)
+toUpdate.name = req.body.name;
 if(req.body.type) toUpdate.type = req.body.type;
 if(req.body.cost) toUpdate.cost = req.body.cost;
 let result = await toUpdate.save();
@@ -45,40 +56,40 @@ res.send(`{"error": ${err}: Update for id ${req.params.id}
 failed`);
 }
 };
-
-//List of all Abiotic
-exports.Abiotic_list = async function (req, res) {
-    try {
-        theAbiotic = await Abiotic.find();
-        res.send(theAbiotic);
+ 
+ 
+exports.Abiotic_list = async function(req, res) {
+    try{
+    theAbiotic = await Abiotic.find();
+    res.send(theAbiotic);
     }
-    catch (err) {
-        res.status(500);
-        res.send(`{"error": ${err}}`);
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
     }
-};
-
-exports.Abiotic_view_all_Page = async function (req, res) {
-    try {
-        theAbiotics = await Abiotic.find();
-        res.render('Abiotic', { title: 'Abiotic Search Results', results: theAbiotics });
+   };
+ 
+ 
+exports.Abiotic_view_all_Page = async function(req, res) {
+    try{
+    theAbiotics = await Abiotic.find();
+    res.render('Abiotic', { name: 'Abiotic Search Results', results: theAbiotics });
     }
-    catch (err) {
-        res.status(500);
-        res.send(`{"error": ${err}}`);
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
     }
-};
-
-
-// Handle Abiotic create on POST.
+    };
+ 
+ 
+    // Handle Abiotic create on POST.
 exports.Abiotic_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Abiotic();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"Abiotic_type":"goat", "cost":12, "size":"large"}
-    document.name= req.body.name;
+    document.name = req.body.name;
     document.type = req.body.type;
     document.cost = req.body.cost;
     try{
@@ -90,6 +101,62 @@ exports.Abiotic_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
-   
-   
+ //Handle a show one view with id specified by query
+ exports.Abiotic_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Abiotic.findById( req.query.id)
+    res.render('Abioticdetail',
+    { title: 'AbioticDetail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    //Handle building the view for creating a Abiotic.
+    // No body, no in path parameter, no query.
+    // Does not need to be async
+    exports.Abiotic_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Abioticcreate', { title: 'Abiotic Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle building the view for updating a Abiotic.
+// query provides the id
+exports.Abiotic_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Abiotic.findById(req.query.id)
+    res.render('Abioticupdate', { title: 'Abiotic Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle a delete one view with id from query
+exports.Abiotic_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Abiotic.findById(req.query.id)
+    res.render('Abioticdelete', { title: 'Abiotic Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+    
+
  
--- routes/Abiotic.js
+++ routes/Abiotic.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const Abiotic_controllers= require('../controllers/Abiotic');
 var router = express.Router();
-/* GET Abiotic */
+/* GET costumes */
 router.get('/', Abiotic_controllers.Abiotic_view_all_Page );
+/* GET detail food page */
+router.get('/detail', Abiotic_controllers.Abiotic_view_one_Page);
+//* GET create costume page */
+router.get('/create', Abiotic_controllers.Abiotic_create_Page);
+/* GET create update page */
+router.get('/update', Abiotic_controllers.Abiotic_update_Page);
+/* GET delete costume page */
+router.get('/delete', Abiotic_controllers.Abiotic_delete_Page);
+
+
 module.exports = router;
